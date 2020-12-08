3bd9bde0acf7cdf3955a8f2139a1e6b6629dcec1
Chunhua (Leo) Liao <liao6@llnl.gov>
2020-12-07 16:27:35 -0800

    more AST examples

---
+++ ada/constants-v2.ads
@@ -0,0 +1,4 @@
+package Constantsv2 is
+  Avogadros_Number  : constant := 6.022137 * 10**23;  -- named number
+  -- Buffer_Size10     : constant := 5_120;
+end Constantsv2; 
---
+++ ada/constants-v3.ads
@@ -0,0 +1,4 @@
+package Constants is
+  Bytes_Per_Page: constant  := 512;
+  Pages_Per_Buffer: constant  Integer := 10;
+end Constants;
