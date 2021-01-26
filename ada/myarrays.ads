6229dae603637a444f36a4b39670a6231737a095
Chunhua (Leo) Liao <liao6@llnl.gov>
2021-01-26 11:51:06 -0800

    (ada) array of 1 or 2 dimensions

---
+++ ada/myarrays.ads
@@ -0,0 +1,11 @@
+package myarrays is
+
+  type Vector is array (Integer range <>) of Float;
+  type Matrix is array (Integer range <>, Integer range <>) of Float; 
+
+-- TODO: fix error  
+--  function "+"  
+--        (Left  : in Vector;  
+--         Right : in Vector) return Vector;
+
+end myarrays; 
