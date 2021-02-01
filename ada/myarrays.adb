e1bd41a0bc134d90126dbea818cb9150cca3fd0a
Chunhua (Leo) Liao <liao6@llnl.gov>
2021-02-01 15:59:55 -0800

    more

--- ada/myarrays.adb
+++ ada/myarrays.adb
@@ -6,11 +6,7 @@ package body myarrays is
              Right : in Vector) return Vector is
 
     Result : Vector (Left'Range) := Left;
-
   begin
-
-    -- Add as many individual elements of the input vectors together as are contained in
-    -- the input vector LEFT to compute the resulting vector and return the result to the caller.
     for I in Result'Range loop
       Result (I) := Result (I) + Right (I);
     end loop;
@@ -20,28 +16,26 @@ package body myarrays is
   end "+";
 
 -- Matrix addition
-  function "+"  
-             (Left  : in Matrix;  
-              Right : in Matrix) return Matrix is
- 
-     Result : Matrix (Left'Range (1),  
-                      Left'Range (2)) := Left;
- 
-   begin 
- 
-     -- Add as many individual elements of the input matrices together as are contained in
-     -- the input matrix LEFT to compute the resulting matrix and return the result to the caller.
-     for Row in Result'Range (1) loop
-       Loop_Column:
-         for Column in Result'Range (2) loop
-           Result (Row, Column) := Result (Row, Column) + Right (Row, Column);
-         end loop Loop_Column;
- 
-     end loop;
- 
-     return Result;
- 
-   end "+";
+--  function "+"  
+--             (Left  : in Matrix;  
+--              Right : in Matrix) return Matrix is
+-- 
+--     Result : Matrix (Left'Range (1),  
+--                      Left'Range (2)) := Left;
+-- 
+--   begin 
+-- 
+--     for Row in Result'Range (1) loop
+--       Loop_Column:
+--         for Column in Result'Range (2) loop
+--           Result (Row, Column) := Result (Row, Column) + Right (Row, Column);
+--         end loop Loop_Column;
+-- 
+--     end loop;
+-- 
+--     return Result;
+-- 
+--   end "+";
 
 
 end myarrays; 
--- ada/myarrays.ads
+++ ada/myarrays.ads
@@ -1,14 +1,14 @@
 package myarrays is
 
   type Vector is array (Integer range <>) of Float;
-  type Matrix is array (Integer range <>, Integer range <>) of Float; 
+--  type Matrix is array (Integer range <>, Integer range <>) of Float; 
 
   function "+"  
         (Left  : in Vector;  
          Right : in Vector) return Vector;
 
-  function "+"  
-            (Left  : in Matrix;  
-             Right : in Matrix) return Matrix;
+--  function "+"  
+--            (Left  : in Matrix;  
+--             Right : in Matrix) return Matrix;
 
 end myarrays; 
