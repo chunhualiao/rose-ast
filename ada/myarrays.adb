01f8e355c28a7029ff492ae5a1cd22efade57dc2
Chunhua Liao <liao6@llnl.gov>
2021-02-22 11:21:03 -0800

    full demo

--- ada/myarrays.adb
+++ ada/myarrays.adb
@@ -16,4 +16,25 @@ package body myarrays is
 
   end "+";
 
+-- Matrix addition
+   function "+"
+              (Left  : in Matrix;
+               Right : in Matrix) return Matrix is
+ 
+      Result : Matrix (Left'Range (1),
+                       Left'Range (2)) := Left;
+ 
+    begin
+ 
+      for Row in Result'Range (1) loop
+        Loop_Column:
+          for Column in Result'Range (2) loop
+            Result (Row, Column) := Result (Row, Column) + Right (Row, Column);
+          end loop Loop_Column;
+ 
+      end loop;
+ 
+      return Result;
+ 
+    end "+";
 end myarrays; 
--- ada/myarrays.ads
+++ ada/myarrays.ads
@@ -1,14 +1,18 @@
 package myarrays is
+  -- 64 bit double precision floating point.
+  subtype Double_Type is Standard.Long_Float;
+
+  subtype Scalar is Double_Type;
 
   type Vector is array (Integer range <>) of Float;
---  type Matrix is array (Integer range <>, Integer range <>) of Float; 
+  type Matrix is array (Integer range <>, Integer range <>) of Float; 
 
   function "+"  
         (Left  : in Vector;  
          Right : in Vector) return Vector;
 
---  function "+"  
---            (Left  : in Matrix;  
---             Right : in Matrix) return Matrix;
+  function "+"  
+            (Left  : in Matrix;  
+             Right : in Matrix) return Matrix;
 
 end myarrays; 
